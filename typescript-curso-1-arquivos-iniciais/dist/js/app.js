d6289766f4e8c24e64c42a1faa5d6f227debbc4a
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-03-04 05:07:04 -0300

    update curso

--- typescript-curso-1-arquivos-iniciais/dist/js/app.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/app.js
@@ -1,10 +1,8 @@
 import { Negociacao } from "./models/negociacao.js";
-
-const negociacao = new Negociacao(new Date(),10,100)
+const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.data);
 console.log(negociacao.valor);
 console.log(negociacao.quantidade);
 console.log(negociacao.volume);
-negociacao.setValor=12
+negociacao.setValor = 12;
 console.log(negociacao.valor);
-
