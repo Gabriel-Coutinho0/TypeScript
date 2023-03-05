5d94a74b8687e385afbb27d22dda92bc439b5c48
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-03-05 17:34:21 -0300

    update curso

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -1,10 +1,13 @@
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
 import { Negociacao } from "./models/negociacao.js";
+const controler = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controler.adiciona();
+})
 
-const negociacao = new Negociacao(new Date(),10,100)
-console.log(negociacao.data);
-console.log(negociacao.valor);
-console.log(negociacao.quantidade);
+
+const negociacao = new Negociacao(new Date(), 10, 100)
 console.log(negociacao.volume);
-negociacao.setValor=12
-console.log(negociacao.valor);
 
